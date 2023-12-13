426625c4a67fcff86f5b0f2b9a1eefe4eb93dbe7
aalqarnimr <51996693+aalqarnimr@users.noreply.github.com>
2023-12-14 00:01:30 +0300

    blood request and donate, user main page

--- sign_in.js
+++ sign_in.js
@@ -1,12 +1,14 @@
-document.getElementById("signin-form").addEventListener("submit", function(e) {
-    e.preventDefault();
-    const userEmail = document.getElementById("email").value;
-    const userPass = document.getElementById("password").value;
-    if (userEmail === "admin@admin" && userPass === "admin") {
-        window.location.href = "admin-main.html";
-    } else {
-        localStorage.setItem("userEmail", userEmail);
-        localStorage.setItem("userPass", userPass);
-        window.location.href = "main-page.html";
-    }
+document.getElementById("signin-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const userEmail = document.getElementById("email").value;
+  const userPass = document.getElementById("password").value;
+  if (userEmail === "admin@admin" && userPass === "admin") {
+    localStorage.setItem("userEmail", userEmail);
+    localStorage.setItem("userPass", userPass);
+    window.location.href = "admin-main.html";
+  } else {
+    localStorage.setItem("userEmail", userEmail);
+    localStorage.setItem("userPass", userPass);
+    window.location.href = "main-page.html";
+  }
 });
