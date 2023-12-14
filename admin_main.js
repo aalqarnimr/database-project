ac243d47a0e7e6502713fca04a1335089d85f6ff
aalqarnimr <51996693+aalqarnimr@users.noreply.github.com>
2023-12-14 19:06:55 +0300

    admin can now add users

--- admin_main.js
+++ admin_main.js
@@ -1,36 +1,60 @@
 otherReportsButtons = document.querySelector(".other-buttons");
 otherReportsTable = document.querySelector(".otherReportsTable");
+//---------------------------database functions-------------------------------------------------------
+const supabaseUrl = "https://vrvtmqckywwkrakjciyq.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZydnRtcWNreXd3a3Jha2pjaXlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDIzMzY3NTQsImV4cCI6MjAxNzkxMjc1NH0.TC_jAS54Muu2Nz0tjVrryX1tyqrcA05OHTu5KaUEWck";
 
-function showCollectionDrives(){
-    otherReportsTable.innerHTML =  `<div class="d-r_header">
+var database = supabase.createClient(supabaseUrl, supabaseKey);
+localStorage.setItem("database", database);
+async function getPersonData() {
+    try {
+      var { data, error } = await database.from('PERSON').select("*");
+        
+      if (error) {
+        throw error;
+      }
+  
+      console.log('Data from table:', data);
+    } catch (error) {
+      console.error('Error fetching data:', error.message);
+    }
+  }
+
+//---------------------------database functions-------------------------------------------------------
+
+
+function showCollectionDrives() {
+    console.log(data);
+  otherReportsTable.innerHTML = `<div class="d-r_header">
     <i>Drive ID</i>
     <i>Total blood Recieved</i>
     <i>Start Date</i>
     <i>End Date</i>
     </div>`;
-    collectionDriveRow = document.createElement("div");
-    collectionDriveRow.className = "1223";
-    collectionDriveRow.innerHTML = `<i>1223</i>
+  collectionDriveRow = document.createElement("div");
+  collectionDriveRow.className = "1223";
+  collectionDriveRow.innerHTML = `<i>1223</i>
     <i>27</i>
     <i>
         12/01/2023
     </i>
     <i>
         12/31/2023
-    </i>`
-    otherReportsTable.append(collectionDriveRow);
+    </i>`;
+  otherReportsTable.append(collectionDriveRow);
 }
-function showConfirmedPayments(){
-    otherReportsTable.innerHTML =  `<div class="d-r_header"> <i>Donor  Name</i>
+function showConfirmedPayments() {
+  otherReportsTable.innerHTML = `<div class="d-r_header"> <i>Donor  Name</i>
     <i>User ID</i>
     <i>Blood Type</i>
     <i>Requested Amount</i>
     <i>Paid (SAR):</i>
     </div>
     `;
-    confirmedPaymentRow = document.createElement("div");
-    confirmedPaymentRow.className = "1114927923";
-    confirmedPaymentRow.innerHTML = `<i>Raid qahtani</i>
+  confirmedPaymentRow = document.createElement("div");
+  confirmedPaymentRow.className = "1114927923";
+  confirmedPaymentRow.innerHTML = `<i>Raid qahtani</i>
     <i>1114927923</i>
     <i>
         O+
@@ -41,10 +65,8 @@ function showConfirmedPayments(){
     <i>
         3000
     </i>
-    `
-    otherReportsTable.append(confirmedPaymentRow);
+    `;
+  otherReportsTable.append(confirmedPaymentRow);
 }
 
-function addUser(){
-    
-}
+function addUser() {}
